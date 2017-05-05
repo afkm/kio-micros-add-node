894c2a9bb178fe54a384da5a3e172696ca571bee
sandbender3000 <christian@afkm.io>
2017-05-05 18:00:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const Path = require('path');
+
+var neo4j = require('neo4j-driver').v1;
+var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "lalala"));
+
+driver.onCompleted = function() {
+  console.log('Successfully connected to Neo4J');
+};
+
+driver.onError = function(error) {
+  console.log('Neo4J Driver instantiation failed', error);
+};
+
+var session = driver.session();
+
+
+require('seneca')()
+  .use('seneca-amqp-transport')
+  .add('cmd:addNode,cuid:*,nodeType:*,nodeTitle:*', function(message, done) {
+    var queryString = "MERGE (" + message.cuid + ":" + message.nodeType + " { cuid:'" + message.cuid + "', title:'" + message.nodeTitle + "' })";
+    console.log(queryString);
+    session
+      .run(queryString)
+      .then(function(result) {
+        session.close();
+        var status = "Successfully added Node " + message.cuid;
+        return done(null, {
+          status
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  })
+  .listen({
+    type: 'amqp',
+    pin: 'cmd:addNode,cuid:*,nodeType:*,nodeTitle:*',
+    url: process.env.AMQP_URL
+  });
